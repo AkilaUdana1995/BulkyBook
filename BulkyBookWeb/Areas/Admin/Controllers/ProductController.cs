8d14ce81b936d5c959a1b40bf06afd3d77943a27
AkilaUdana1995 <Udanakill@gmail.com>
2022-10-21 11:33:40 +0530

    updates

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -5,58 +5,61 @@ using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 
 namespace BulkyBookWeb.Areas.Admin.Controllers;
 [Area("Admin")]
 public class ProductController : Controller
-       
-{
-        // private readonly ApplicationDbContext _db;   //interchanging dbContext with Repository
-        private readonly IUnitOfWork _db;
 
-        public ProductController(IUnitOfWork db)
+    {
+    // private readonly ApplicationDbContext _db;   //interchanging dbContext with Repository
+    private readonly IUnitOfWork _db;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public ProductController(IUnitOfWork db, IWebHostEnvironment webHostEnvironment)
         {
-            _db = db;
+        _db = db;
+        _webHostEnvironment = webHostEnvironment;
         }
-        public IActionResult Index()
+    public IActionResult Index()
         {
-            IEnumerable<Product> objCategoryList = _db.Product.GetAll();  //_db then get db set name
-            return View(objCategoryList);
+        IEnumerable<Product> objCategoryList = _db.Product.GetAll();  //_db then get db set name
+        return View(objCategoryList);
         }
 
-        //GET
+    //GET
 
-        //public IActionResult Create()
-        //{
+    //public IActionResult Create()
+    //{
 
-        //    return View();
-        //}
+    //    return View();
+    //}
 
-        //POST
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Create(Product obj)
-        //{
+    //POST
+    //[HttpPost]
+    //[ValidateAntiForgeryToken]
+    //public IActionResult Create(Product obj)
+    //{
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        _db.Product.Add(obj);                     //_db.Categories.Add(obj);
-        //        _db.Save();                            //_db.SaveChanges();
-        //        TempData["success"] = "Product Added Succesfully!!";
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(obj);
+    //    if (ModelState.IsValid)
+    //    {
+    //        _db.Product.Add(obj);                     //_db.Categories.Add(obj);
+    //        _db.Save();                            //_db.SaveChanges();
+    //        TempData["success"] = "Product Added Succesfully!!";
+    //        return RedirectToAction("Index");
+    //    }
+    //    return View(obj);
 
-        //}
+    //}
 
 
-        //GET
+    //GET
 
 
     //upsert method(update + insert)   logic, if id is null or empty, then we create new product object, else we will update it....
-        public IActionResult Upsert(int? id)
+    public IActionResult Upsert(int? id)
         {
         //Product product=new ();
         //IEnumerable<SelectListItem> CategoryList = _db.Category.GetAll().Select(
@@ -96,7 +99,7 @@ public class ProductController : Controller
                }
            )
 
-        };
+            };
 
 
         if (id == null || id == 0)
@@ -104,69 +107,86 @@ public class ProductController : Controller
             //ViewBag.CategoryList = CategoryList;
             //ViewData["CoverTypeList"] = CoverTypeList;
             return View(productVM);
-               //craete product
+            //craete product
             }
 
         else
             {
             //update product
             }
-         
-           
-            return View(productVM);
+
+
+        return View(productVM);
         }
 
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Upsert(ProductVM obj , IFormFile file)
-        {
-
-        
-            if (ModelState.IsValid)
-            {
-              //  _db.CoverType.Update(obj);
-                _db.Save();
-                TempData["success"] = "CoverType Updated Succesfully!!";
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-
-        }
-
-        //method to delete object
-
-        //GET
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            //   var categoryFromDb = _db.Categories.Find(id);
-            var categoryDbFirst = _db.Product.GetFirstOrDefault(u => u.Id == id);
-            if (categoryDbFirst == null)
-            {
-                return NotFound();
-            }
-            return View(categoryDbFirst);
-        }
-
-        //POST
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Delete(Product obj)
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Upsert(ProductVM obj, IFormFile? file)
         {
 
 
-            if (ModelState.IsValid)
+        if (ModelState.IsValid)
             {
-                _db.Product.Remove(obj);
-                _db.Save();
-                TempData["success"] = "Category Deleted Succesfully!!";
-                return RedirectToAction("Index");
+            var wwwRootPath = _webHostEnvironment.WebRootPath;
+            if (file != null)
+                {
+                string fileName = Guid.NewGuid().ToString();
+                var uploads = Path.Combine(wwwRootPath, @"images\products");
+                var extention = Path.GetExtension(file.FileName);
+
+                using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extention), FileMode.Create))
+                    {
+                    file.CopyTo(fileStreams);
+                    }
+
+                obj.Product.ImageURL = @"\images\Products" + fileName + extention;
+                }
+
+
+
+            _db.Product.Add(obj.Product);
+            _db.Save();
+            TempData["success"] = "Product Created  Succesfully!!";
+            return RedirectToAction("Index");
             }
-            return View(obj);
+        return View(obj);
+
+        }
+
+    //method to delete object
+
+    //GET
+    public IActionResult Delete(int? id)
+        {
+        if (id == null || id == 0)
+            {
+            return NotFound();
+            }
+        //   var categoryFromDb = _db.Categories.Find(id);
+        var categoryDbFirst = _db.Product.GetFirstOrDefault(u => u.Id == id);
+        if (categoryDbFirst == null)
+            {
+            return NotFound();
+            }
+        return View(categoryDbFirst);
+        }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(Product obj)
+        {
+
+
+        if (ModelState.IsValid)
+            {
+            _db.Product.Remove(obj);
+            _db.Save();
+            TempData["success"] = "Category Deleted Succesfully!!";
+            return RedirectToAction("Index");
+            }
+        return View(obj);
 
         }
     }
